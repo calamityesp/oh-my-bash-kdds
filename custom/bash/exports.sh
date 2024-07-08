52da4ec02e85e60206680f9f5f5e9a3218db2074
calamityesp <calamity_xemus@hotmail.com>
2024-07-08 13:12:58 -0600

    necessary

--- custom/bash/exports.sh
+++
@@ -1,2 +0,0 @@
-export PATH="$PATH:/home/calamityesp/.asdf/installs/golang/1.22.3/go/bin:/home/calamityesp/.asdf/installs/golang/1.22.3/packages/bin"
-export obsidian_path="/home/calamityesp/AppImages/obsidian/Obsidian.appimage"
